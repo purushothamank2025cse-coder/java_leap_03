d9a58bcaa25b4ea579cfbb68a904b76de34cc991
purushothamank2025cse-coder <purushothaman.k2025cse@sece.ac.in>
2026-09-18 15:33:47 +0530

    Updated project

--- src/main/java/com/example/SpringJPA/Model/Food.java
+++ src/main/java/com/example/SpringJPA/Model/Food.java
@@ -1,7 +1,7 @@
 package com.example.SpringJPA.Model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,11 +16,11 @@ import lombok.NoArgsConstructor;
 
 public class Food {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     Long id;
     String foodname;
     double price;
     @Column(name = "is_available")
     boolean available;
-
+    
 }
